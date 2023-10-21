79b6ee94bad5b71a2527476035e1eef271ff1b0e
tomasz <t.j.tomasz@gmail.com>
2023-10-21 14:07:51 +0200

    Add add emplyee screen, some formatting and first routes.

--- lib/screen/add_employee_screen.dart
+++ lib/screen/add_employee_screen.dart
@@ -8,12 +8,45 @@ class AddEmployeeScreen extends StatefulWidget {
 }
 
 class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
+
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add employee'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              //TODO
+              //funkcja dodania do bazy
+            },
+            icon: const Icon(Icons.save)
+            )
+        ],
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _controller,
+              keyboardType: TextInputType.name,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('Employee Name')
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Name cant't be empty";
+                }
+              },
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,23 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+
+      body: Column (
+        children: [
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.pushNamed(context, '/add_employee');
+        },
+        icon: const Icon(Icons.add),
+        label: const Text('Add employee')),
+    );
   }
 }
