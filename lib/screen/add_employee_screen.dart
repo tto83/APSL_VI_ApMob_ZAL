eb84b61d764bb32eea76d143706f8f14dcbfa32c
tomasz <t.j.tomasz@gmail.com>
2023-10-23 16:54:42 +0200

    Add other data fields.

--- lib/screen/add_employee_screen.dart
+++ lib/screen/add_employee_screen.dart
@@ -10,7 +10,10 @@ class AddEmployeeScreen extends StatefulWidget {
 
 class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
 
-  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _employeeNameController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _dobController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +36,13 @@ class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            CustomTextFormField(controller: _controller, txtLabel: 'Employee name',),
+            CustomTextFormField(controller: _employeeNameController, txtLabel: 'Employee name',),
+            const SizedBox(height: 8.0,),
+            CustomTextFormField(controller: _firstNameController, txtLabel: 'First name',),
+            const SizedBox(height: 8.0,),
+            CustomTextFormField(controller: _lastNameController, txtLabel: 'Last name',),
+            const SizedBox(height: 8.0,),
+            CustomTextFormField(controller: _dobController, txtLabel: 'Date of birth',),
             const SizedBox(height: 8.0,),
           ],
         ),
